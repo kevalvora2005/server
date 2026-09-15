ca463eed67454b1a2a441806f91e025803cea9c3
Keval Vora <keval.vora@tatvasoft.com>
2026-09-15 10:31:13 +0530

    refactor: remove PasswordResetToken and related repository and model for simplification

--- src/modules/auth/domain/repositories/IPasswordResetTokenRepository.ts
+++
@@ -1,8 +0,0 @@
-import { PasswordResetToken } from "../entities/PasswordResetToken";
-
-export interface IPasswordResetTokenRepository {
-  create(token: PasswordResetToken): Promise<PasswordResetToken>;
-  findByToken(token: string): Promise<PasswordResetToken | null>;
-  deleteByUserId(userId: number): Promise<void>;
-  deleteByToken(token: string): Promise<void>;
-}
