7889714038f637df7b34451bca3c10c0bc31d1be
Keval Vora <keval.vora@tatvasoft.com>
2026-09-21 10:34:04 +0530

    feat: simplify language detection in internationalization middleware

--- src/shared/middleware/i18nMiddleware.ts
+++ src/shared/middleware/i18nMiddleware.ts
@@ -2,10 +2,11 @@ import { Request, Response, NextFunction } from "express";
 
 export const SUPPORTED_LANGUAGES = ["en", "hi", "gu"];
 
-export const i18nMiddleware = (req: Request, _res: Response, next: NextFunction) => {
-  const header = req.headers["accept-language"] as string;
-  const headerLng = header?.split(",")[0]?.split("-")[0]?.toLowerCase();
+export const i18nMiddleware = (req: Request, res: Response, next: NextFunction) => {
 
-  req.language = SUPPORTED_LANGUAGES.includes(headerLng) ? headerLng : "en";
+  const language = req.headers["accept-language"]?.toLowerCase();
+
+  req.language = SUPPORTED_LANGUAGES.includes(language || "") ? language : "en";
+
   next();
 };
